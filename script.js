6bcd9fd53152b53e1db6d05995a1b52d73266c69
Masa Orel <orel.masha89@gmail.com>
2022-06-03 14:21:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// two integers
+/*
+let a = 8
+let b = 21
+
+if (a > b) {
+  console.log('The larger number is a: ' + a)
+} else {
+  console.log('The larger number is b: ' + b)
+}
+*/
+// or with a function
+
+function larger(num1, num2) {
+    if (num1 > num2) {
+            console.log("The larger number is first: " + num1);
+        } else {
+            console.log("The larger number is second: " + num2);
+    }
+};
+
+larger(21, 99);
+
+// odd and even numbers
+
+// for(let i = 1; i < 10; i++) {
+//     if(i % 2 !== 0) {
+//         console.log("Numbers are odd: ", i)
+//     }
+//     else {
+//         console.log("Numbers are even: ", i)
+//     }
+// }
